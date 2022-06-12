2e45227863cbada6499ee337717beb51daed0620
Anurag <sanurag4518@gmail.com>
2022-06-12 16:35:10 +0530

    tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-"./utils/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
+  plugins: [],
   enabled: process.env.NODE_ENV === "production",
-}
-
+};
